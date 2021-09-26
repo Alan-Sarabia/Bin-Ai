ea945c15740394c19ca842918fd902f532fd720c
144LM <144lucasm123@gmail.com>
2021-09-25 21:37:13 -0300

    new footer

--- react-front/src/components/pages/Footer/Footer.js
+++ react-front/src/components/pages/Footer/Footer.js
@@ -1,76 +1,129 @@
 import React from 'react';
-import './Footer.scss';
+import './Footer.css';
+
 import { Link } from 'react-router-dom';
 import {
+  FaFacebook,
   FaInstagram,
   FaYoutube,
   FaTwitter,
-  FaLinkedin,
- 
+  FaLinkedin
 } from 'react-icons/fa';
-import { RiRecycleFill } from 'react-icons/ri';
-
+import { MdFingerprint } from 'react-icons/md';
 
 function Footer() {
-  
   return (
-   
-  
-
-    
-
-<footer>
-  <div class="centered clearfix">
-    
-    
-    <div class="footer-contact">
-       <h3><a href="">Contactos</a></h3>
-       <p><a href="mailto:">fake@fake.com</a></p>
-       <p><a href="tel:">123-456-7890</a></p>
-       <p><a href="">123 Fake St<br />Fake, PA 11111</a></p>
+    <div className='footer-container'>
+      <section className='footer-subscription'>
+        <p className='footer-subscription-heading'>
+          Join our exclusive membership to receive the latest news and trends
+        </p>
+        <p className='footer-subscription-text'>
+          You can unsubscribe at any time.
+        </p>
+        <div className='input-areas'>
+          <form>
+            <input
+              className='footer-input'
+              name='email'
+              type='email'
+              placeholder='Your Email'
+            />
+            
+          </form>
+        </div>
+      </section>
+      <div className='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div className='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div className='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
+      </div>
+      <section className='social-media'>
+        <div className='social-media-wrap'>
+          <div className='footer-logo'>
+            <Link to='/' className='social-logo'>
+              <MdFingerprint className='navbar-icon' />
+              BIN AI
+            </Link>
+          </div>
+          <small className='website-rights'>BIN AI© 2021</small>
+          <div className='social-icons'>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Facebook'
+            >
+              <FaFacebook />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Instagram'
+            >
+              <FaInstagram />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to={
+                '//www.youtube.com/channel/UCsKsymTY_4BYR-wytLjex7A?view_as=subscriber'
+              }
+              target='_blank'
+              aria-label='Youtube'
+            >
+              <FaYoutube />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='Twitter'
+            >
+              <FaTwitter />
+            </Link>
+            <Link
+              className='social-icon-link'
+              to='/'
+              target='_blank'
+              aria-label='LinkedIn'
+            >
+              <FaLinkedin />
+            </Link>
+          </div>
+        </div>
+      </section>
     </div>
-    <div class="footer-navigation">
-      <div class="footer-links-holder">
-        <h3><a href="">Section 1</a></h3>
-        <ul class="footer-links">
-          <li><a href="">Page Title 1</a></li>
-          <li><a href="">Page Title 2</a></li>
-          <li><a href="">Page Title 3</a></li>
-          <li><a href="">Page Title 4</a></li>
-        </ul>
-      </div>
-      <div class="footer-links-holder">
-        <h3><a href="">Section 2</a></h3>
-        <ul class="footer-links">
-          <li><a href="">Page Title 1</a></li>
-          <li><a href="">Page Title 2</a></li>
-          <li><a href="">Page Title 3</a></li>
-          <li><a href="">Page Title 4</a></li>
-        </ul>
-      </div>
-      <div class="footer-links-holder">
-        <h3><a href="">Section 3</a></h3>
-        <ul class="footer-links">
-          <li><a href="">Page Title 1</a></li>
-          <li><a href="">Page Title 2</a></li>
-          <li><a href="">Page Title 3</a></li>
-          <li><a href="">Page Title 4</a></li>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <div class="bottom-bar">
-      All Rights Reserved © 2021 | <a href="">Privacy Policy</a> | <a href="">Terms of Service</a>
-  </div>
-</footer>
-
-
-    
-    
-    
-  
-      
-       
   );
 }
 

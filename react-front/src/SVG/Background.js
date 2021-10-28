4eeb4f005f996d81895c146b3f27ff9f0e9b6e6b
144LM <144lucasm123@gmail.com>
2021-10-28 14:57:47 -0300

    New background

---
+++ react-front/src/SVG/Background.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import './Background.css';
+//import b from './b.svg';
+
+function Background() {
+    
+    return (
+        <div >
+            <div class="spacer layer1" > 
+           
+          
+    
+    <section>
+      <h1>Eco IA</h1>
+      <p></p>
+    </section>
+
+    <section class="bubble">
+    <br/>
+    <h1>Cargar el modelo</h1>
+    {/*boton */}
+    </section>
+    <section class="bubble">
+     
+      
+    </section>
+    
+    <section class="dark">
+     
+    </section>
+
+    
+
+                <div class="spacer layer2" > 
+                
+    
+                </div>
+            </div>
+        </div> 
+    )
+}
+
+export default Background
